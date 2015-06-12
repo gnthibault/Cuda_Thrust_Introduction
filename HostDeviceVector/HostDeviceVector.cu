52c6dfde033561a8062fb9c33e726d330adcd511
thibault.notargiacomo <thibault.notargiacomo@thalesgroup.com>
2015-06-12 13:53:54 +0200

    First step in building a proper tutorial project: CMakeLists.txt and toy example

---
+++ HostDeviceVector/HostDeviceVector.cu
@@ -0,0 +1,20 @@
+/*
+ * HostDeviceVector.cu
+ *
+ *  Created on: 12 juin 2015
+ *      Author: Thibault Notargiacomo
+ */
+
+//STL
+#include <cstdlib>
+
+//Thrust
+#include "thrust/device_vector.h"
+#include "thrust/host_vector.h"
+
+
+int main( int argc, char* argv[] )
+{
+
+	return EXIT_SUCCESS;
+}
